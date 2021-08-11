845f38d47bcc4eba59ec3565f28938be70e39de3
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-08-11 16:33:07 +0300

    Add the number from a string where the maximum number of numbers is two

---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        public int Add(string stringOfNumbers)
+        {
+            switch (stringOfNumbers.Length)
+            {
+                case 1: return Int32.Parse(stringOfNumbers);
+                case 3: return (int)(Char.GetNumericValue(stringOfNumbers[0]) + Char.GetNumericValue(stringOfNumbers[2]));
+                default: return 0;
+            }
+        }
+    }
+}
