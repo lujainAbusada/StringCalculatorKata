ecaf6ba2453da6a2001a9c726f1851b97b01917a
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-08-12 15:11:09 +0300

    Support different delimiters: to change a delimiter, the beginning of the string will contain a separate line

---
+++ StringCalculatorKata/DelimeterExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorKata
+{
+    public class DelimeterExtractor
+    {
+        public virtual List<char> FindDelimeter(string stringOfNumbersAndDelimeter)
+        {
+            return new List<char> {stringOfNumbersAndDelimeter.Split("\n")[0].Last(), ',', '\n' };
+        }
+    }
+}
